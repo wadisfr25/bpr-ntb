c509264f7f3d07568e1d3c8c7ea5fe750045df1d
Fransi25 <wadisfr25@gmail.com>
2026-01-07 22:00:45 +0800

    salah hapus baris code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -878,7 +878,7 @@
     @include('partials.footer')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
+    @stack('scripts')
 </body>
 
 </html>
